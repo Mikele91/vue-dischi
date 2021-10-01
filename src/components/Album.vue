0539e612fd759586d38b5adbf043a74da565bf0d
Mikele91 <michele.amoruso91.8@gmail.com>
2021-10-01 17:51:53 +0200

    aggiunti album

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,16 +1,32 @@
 <template>
-    <div>
-
+      <div class="album text-center">
+        <img class="py-3" :src= "info.poster" alt="info.title">
+        <h3>{{info.title}}</h3>
+        <div>{{info.author}}</div>
+        <div>{{info.year}}</div>
     </div>
 </template>
 
 <script>
 export default {
     name: "Album",
+    props:{
+        info: Object
+
+    }
 
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/style/variables.scss';
+
+.album{
+    background-color: $headerColor;
+    height: 300px;
+}
+img{
+    max-width: 60%;
+}
 
 </style>
--- src/components/Albums.vue
+++ src/components/Albums.vue
@@ -1,21 +1,44 @@
 <template>
-<div>
-    <Album/>
+<div class="container my-5"> 
+    <div class="row row-cols-5">
+        <div class="col mb-3" v-for= "(elm, index) in album" :key="index"> 
+            <Album :info="elm" />
+        </div>
+        
+    </div>
+    
 
 </div>
 </template>
 
 <script>
-// import axios from 'axios';
+import axios from 'axios';
 import Album from './Album.vue'
 export default { 
     name: "Albums",
     components: {
         Album,
     },
+    data(){
+        return{
+            album:[]
+        }
+        
+    },
+    // chimata al server tramite axios 
+    created(){
+        axios
+        .get("https://flynn.boolean.careers/exercises/api/array/music")
+        .then( (res)=> {
+            this.album= res.data.response ;
+        })
+    }
 }
 </script>
 
 <style  lang="scss" scoped>
+.container{
+    max-width: 75rem;
+}
 
 </style>
