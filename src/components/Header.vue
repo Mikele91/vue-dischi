670e0ac4a4f113163c128bb6effb756741af127b
Mikele91 <michele.amoruso91.8@gmail.com>
2021-10-01 17:13:35 +0200

    struttura base

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="">
+       <img src="../assets/images/spotify-logo.png" alt="" class="m-2">
+  </header>
+</template>
+
+<script>
+export default {
+name: "Header"
+
+}
+</script>
+
+<style  lang="scss" scoped>
+@import '../assets/style/variables.scss';
+header{
+    height: 6.25rem;
+    background-color: $headerColor;
+    img{
+        height: 80%;
+    }
+}
+
+</style>
